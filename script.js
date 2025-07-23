112e1dbe0cd08b0a0553d41b13c9c7111157a4e5
lauryn <laurynmwase06@gmail.com>
2025-07-23 12:17:34 +0200

    created the library page and styles it

---
+++ script.js
@@ -0,0 +1,41 @@
+
+// function book(tittle, author, pages,Read){
+//     this.tittle=tittle;
+//     this.author=author;
+//     this.pages=pages;
+//     this.Read=Read? "IS READ" : "IS NOT READ";
+
+//  this.info= info=()=>{
+
+// return(`The book ${tittle} by ${author} with ${pages} `)
+// }
+// };
+
+// const x=new book("I don't care anymore","lala",100);
+// console.log(x.info());
+// const h=document.querySelector(".hh");
+// h.textContent=x.info();
+// const title=document.querySelector();
+// const autho=document.querySelector();
+// const page=document.querySelector();
+// const myLibrary=[
+//     ["lala","kaya",100],
+//     []
+// ];
+// function Books{
+//     this.tittle=tittle;
+//     this.author=author;
+//     this.pages=pages;
+// }
+// AddSomeBooksToLibrary=(title,autho,page)=>{
+
+// }
+// for(let i=0;i<navigator;i++){
+//     console.log(myLibrary[Books])
+// }
+
+
+// submit.addEventListener("click",()=>{
+//     if()
+//     document.location="index.html";
+// })
